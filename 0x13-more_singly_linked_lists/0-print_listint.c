168685b8fc35e205ded3351412b3ef8a5129815c
alxprincess <akuwudikeprincess@gmail.com>
2023-10-24 19:16:39 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of a linked list
+ * print_listint - to print all the elements of a linked list
  * @a: linked list of type listint_t to print
  *
  * Return: the number of nodes
